30461589535469780925e3183895562d6f23d506
bachalakurirevanth <bachalakurirevanth123@gmail.com>
2025-01-30 12:03:58 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,15 @@
+package bhagirath;
+import java.util.*;
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		String s=sc.next();
+		int S= s.length();
+		System.out.println(s);
+		System.out.println("String Length: " + S);
+
+	}
+
+}
